72ee8a1aa08beda4f0c154e1227f72b452df7134
t11235813213455-t <t11235813213455@gmail.com>
2025-11-10 09:56:58 +0300

    add test_main_page.py, conftest.py, edit base_page.py and main_page.py

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,8 +1,8 @@
 class BasePage():
-	def __init__(self, browser, url):
-		self.browser = browser
-		self.url = url
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
 
     def open(self):
-    	self.browser.get(self.url)
-    	
+        self.browser.get(self.url)
+
