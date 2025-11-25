5c99ebc1cf23ea46dcd7a959460d769ed33f967d
t11235813213455-t <t11235813213455@gmail.com>
2025-11-25 17:42:45 +0300

    update requirements.txt

--- test_main_page.py
+++ test_main_page.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
 
-    #@pytest.mark.skip
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)    
@@ -19,7 +18,6 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-    #@pytest.mark.skip
     def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = MainPage(browser, link)
